fa69acade97259ce570cacbb623296879d4d4c18
Nurbol167 <ndretlov@bk.ru>
2025-11-17 02:48:33 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,6 +11,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
+        secure: false, // если бэкенд без HTTPS
       },
     },
   },
